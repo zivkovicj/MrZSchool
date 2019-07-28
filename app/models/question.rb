05e8d8b259db171834a2e906d56f582743508094
Jeff Zivkovic <zivkovic.jeff@gmail.com>
2019-07-28 15:37:51 +0000

    Improved Question Method

--- app/models/question.rb
+++ app/models/question.rb
@@ -32,16 +32,38 @@ class Question < ApplicationRecord
         end
         label_to_delete.destroy
       end
-      this_label = Label.create(:name => "Simple Multiply by #{first_num}")
+      label_to_delete = Label.find_by(:name => "Multiply by #{first_num}, lv 2")
+      if label_to_delete.present?
+        label_to_delete.questions.each do |question|
+          question.destroy
+        end
+        label_to_delete.destroy
+      end
+      
+      this_label = Label.create(:name => "Multiply by #{first_num}, Lv 1")
       mult_topic.labels << this_label if !mult_topic.labels.include?(this_label)
-      [0,1,2,3,4,5,6,7,8,9,10,11,12].each do |second_num|
+      [0,1,2,3,4,5,6,7,8,9,10].each do |second_num|
         this_prompt = "#{first_num} * #{second_num} = ____"
         if Question.where(:prompt => this_prompt).count == 0 then
           Question.create(:style => "fill_in", :prompt => this_prompt,
                           :extent => "public", :label => this_label,
-                          :correct_answers => ["#{first_num * second_num}"], :grade_type => "computer")
+                          :correct_answers => ["#{first_num * second_num}"], :grade_type => "computer"
+                          :user_id => 1)
           end
       end
+      
+      this_label = Label.create(:name => "Multiply by #{first_num}, Lv 2")
+      mult_topic.labels << this_label if !mult_topic.labels.include?(this_label)
+      [11,12,15,20,50,100,200,500,1000].each do |second_num|
+        this_prompt = "#{first_num} * #{second_num} = ____"
+        if Question.where(:prompt => this_prompt).count == 0 then
+          Question.create(:style => "fill_in", :prompt => this_prompt,
+                          :extent => "public", :label => this_label,
+                          :correct_answers => ["#{first_num * second_num}"], :grade_type => "computer"
+                          :user_id => 1)
+          end
+      end
+      
     end
   end
   
