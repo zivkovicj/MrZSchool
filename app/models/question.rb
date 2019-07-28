8bd3529fcfddddb5219ae20637fcb0272ec22232
Jeff Zivkovic <zivkovic.jeff@gmail.com>
2019-07-28 01:35:45 +0000

    Method for creating questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -20,6 +20,23 @@ class Question < ApplicationRecord
     prompt[0,45]
   end
   
+  def create_multiplication_questions
+    mult_topic = Topic.find_by(:name => "Multiplication")
+    
+    [0,1,2,3,4,5,6,7,8,9,10].each do |first_num|
+      this_label = Label.find_or_create_by(:name => "Simple Multiply by #{first_num}")
+      mult_topic.labels << this_label if !mult_topic.labels.include?(this_label)
+      [0,1,2,3,4,5,6,7,8,9,10].each do |second_num|
+        this_prompt = "#{first_num} * #{second_num} = ____"
+        if Question.where(:prompt => this_prompt).count == 0 then
+          Question.create(:style => "fill_in", :prompt => this_prompt,
+                          :extent => "public", :label => this_label,
+                          :correct_answers => ["#{first_num * second_num}"], :grade_type => "computer")
+          end
+      end
+    end
+  end
+  
   private
     
 end
